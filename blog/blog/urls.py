c4bdf96a07df9ad79a4dd85ff02d29d4e6a3aa54
Aditya Mewada <eng.adityamewada@gmail.com>
2024-12-08 17:33:30 +0530

    templates

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,5 +20,5 @@ from posts import views , urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
+    path('post/', include('posts.urls'))
 ]
--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import path 
 from . import views
 urlpatterns =[
-    path('home/<str:name>' , views.home , name="home"),
-    path('<int:id>/' , views.post ),
-    path('<int:id>/', views.google)
+    path('home/', views.home , name="home"),
+    path('<int:id>/' , views.post, name = "post" ),
+    path('g/<int:id>/', views.google)
 ]
