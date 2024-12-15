1f86aeee55ab99c6dfbcffee6b868182e80bb2ee
Aditya Mewada <eng.adityamewada@gmail.com>
2024-12-15 14:21:54 +0530

    travelmate fetching data from database

--- travelmate/travelmate/settings.py
+++ travelmate/travelmate/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 STATIC_ROOT = BASE_DIR/'assets'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- travelmate/ttapp/views.py
+++ travelmate/ttapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import destination
 # Create your views here.
 
 def home(request):
-    return render(request, 'travelmate/index.html')
+    dests = destination.objects.all()
+    return render(request, 'travelmate/index.html', {'dests':dests})
